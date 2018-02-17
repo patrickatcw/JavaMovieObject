214e0fc48406de7f33b05facf60a2c32b65b91e8
patrickatcw <pj8392@yahoo.com>
2018-02-17 10:14:15 -0500

    instances for new objects created in Main, need to complete printout logic

--- src/com/me/Main.java
+++ src/com/me/Main.java
@@ -4,6 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //building new instances, creating new objects
+        Movie a = new Movie("The Thing", 1979, "R", "horror", 123);
+        Movie b = new Movie("White Nights", 1987, "R", "drama", 147);
+        Movie c = new Movie("Popeye", 1985, "PG", "comedy", 136);
+        Movie d = new Movie("The Shop", 2012, "PG", "comedy", 106);
+
+        //printout logic
+        System.out.println("Here is some movie information;");
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
     }
 
 }
