25a4397adfbba20d0e0da2fdb70ea6a546f17a83
patrickatcw <pj8392@yahoo.com>
2018-02-16 22:11:29 -0500

    fields created

--- src/com/me/Movie.java
+++ src/com/me/Movie.java
@@ -2,6 +2,14 @@ package com.me;
 
 public class Movie {
 
+    //fields
+    public String name;
+    public int year;
+    public String rating;
+    public String genre;
+    public int length;
+
+    //constructor with parameters
 
 
 }
