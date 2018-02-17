65e1953df9aefa512612ae9bfbead92a37657641
patrickatcw <pj8392@yahoo.com>
2018-02-17 10:04:58 -0500

    added constructor w/ parameters and getter methods in Movie class

--- src/com/me/Movie.java
+++ src/com/me/Movie.java
@@ -3,13 +3,43 @@ package com.me;
 public class Movie {
 
     //fields
-    public String name;
+    public String title;
     public int year;
     public String rating;
     public String genre;
     public int length;
 
     //constructor with parameters
+    public Movie(String title, int year, String rating, String genre,
+                 int length){
 
+        this.title = title;
+        this.year = year;
+        this.rating = rating;
+        this.genre = genre;
+        this.length = length;
+
+    }
+
+    //getter methods for this. fields
+    public String getTitle(){
+        return this.title;
+    }
+
+    public int getYear(){
+        return this.year;
+    }
+
+    public String getRating(){
+        return this.rating;
+    }
+
+    public String getGenre(){
+        return this.genre;
+    }
+
+    public int getLength(){
+        return this.length;
+    }
 
 }
